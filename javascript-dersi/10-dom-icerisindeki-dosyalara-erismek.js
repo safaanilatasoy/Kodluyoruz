8ccb62c21a2b444162a6eeb3db021013933388eb
safaanilatasoy <safaanilatasoy@gmail.com>
2022-12-07 19:28:09 +0300

    010

---
+++ javascript-dersi/10-dom-icerisindeki-dosyalara-erismek.js
@@ -0,0 +1,7 @@
+let title = document.getElementById('title')
+title.innerHTML = "Degisen Bilgi"
+console.log(title.innerHTML)
+
+
+let link = document.querySelector("ul#list>li>a")
+link.innerHTML = 'link bilgisi değişti'
