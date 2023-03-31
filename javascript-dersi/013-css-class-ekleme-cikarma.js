4e39ce11332537d4d1f25a63d71d00d14089d541
safaanilatasoy <safaanilatasoy@gmail.com>
2023-03-31 20:04:07 +0300

    main

---
+++ javascript-dersi/013-css-class-ekleme-cikarma.js
@@ -0,0 +1,14 @@
+// css class eklemek veya çıkartmak
+
+let greeting = document.querySelector("#greeting")
+
+greeting.classList.add("text-primary")
+greeting.classList.add("title");
+greeting.classList.add("new-info", "second-class", "third-class");  // birden fazla eklemek
+
+
+// çıkartmak
+
+greeting.classList.remove("title","second-class", "third-class")    // birden fazla class silmek 
+
+console.log(greeting.classList)
