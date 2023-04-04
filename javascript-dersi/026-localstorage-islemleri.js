e747359b1fa45c8408b5ae00c24784b96f3c5371
safaanilatasoy <safaanilatasoy@gmail.com>
2023-04-05 00:41:58 +0300

    main

--- javascript-dersi/026-localstorage-islemleri.js
+++ javascript-dersi/026-localstorage-islemleri.js
@@ -8,3 +8,8 @@ let userInfo = localStorage.getItem("userInfo")  //bilgiyi al
 userInfo = JSON.parse(userInfo)  //bilgiyi parse et
 console.log(userInfo)
 
+let items = [1,2,3,user]
+
+localStorage.setItem("newItem", JSON.stringify(items))
+localStorage.getItem("newItem")
+JSON.parse(localStorage.getItem("newItem"))
