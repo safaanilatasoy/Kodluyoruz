a7ee9d788f32692e91b5c9f0284da1a66aaed8ad
safaanilatasoy <safaanilatasoy@gmail.com>
2023-04-05 16:21:13 +0300

    main

---
+++ javascript-dersi/029-formlar-bolumsonu-ex.js
@@ -0,0 +1,9 @@
+// FORMLARLA ÇALIŞMAK: BÖLÜM SONU EXERCISES
+
+/*
+1: Formu seç
+2: Input bilgisini UL içine ekle
+3: Form içindeki bilgiyi sıfırla
+4: Eğer forma bilgi girilmezse kullanıcıyı uyar
+*/
+
