4a2cd2a9b9934c233227cfcbbaad58d2cc748cc0
safaanilatasoy <safaanilatasoy@gmail.com>
2023-04-05 16:45:04 +0300

    main

--- javascript-dersi/029-formlar-bolumsonu-ex.js
+++ javascript-dersi/029-formlar-bolumsonu-ex.js
@@ -7,3 +7,25 @@
 4: Eğer forma bilgi girilmezse kullanıcıyı uyar
 */
 
+let userFormDOM = document.querySelector('#userForm')
+userFormDOM.addEventListener('submit', formHandler)
+
+function formHandler(event) {
+    event.preventDefault()
+    const USER_NAME = document.querySelector('#username')
+    const SCORE = document.querySelector('#score')
+    addItem(USER_NAME.value, SCORE.value)
+}
+
+/* <li class="list-group-item d-flex justify-content-between align-items-center">
+  A list item
+  <span class="badge bg-primary rounded-pill">14</span>
+</li>; */
+
+let userListDOM = document.querySelector('#userList')
+const addItem=(username, score) =>   {
+    let liDOM = document.createElement('li')
+    liDOM.innerHTML =`${username} ${score}`
+    userListDOM.append(liDOM)
+
+}
