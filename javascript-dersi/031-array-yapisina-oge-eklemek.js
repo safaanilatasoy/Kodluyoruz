e6f811930f06740fafcbf9e8909ee4d2af857269
safaanilatasoy <safaanilatasoy@gmail.com>
2023-04-12 08:18:37 +0300

    main

---
+++ javascript-dersi/031-array-yapisina-oge-eklemek.js
@@ -0,0 +1,32 @@
+// Array yapısına oge eklemek ve Array içerisinden istenilen ogenin ççıkartılması
+
+
+let items = [10, 20, 30, 40, ]
+console.log("items - ilk hali : ", items)
+
+
+// Array sona oge/eleman eklemek -> push
+items.push(50)
+console.log("50 : ", items);
+
+// Array: basa oge eleman eklemek -> unshift
+items.unshift(5)
+console.log("5 : ",items)
+
+// Array: sondaki ogeyi cikarmak -> pop
+let lastItem = items.pop() // son elemanı lastItem içerisine ekledik
+console.log(lastItem, items)
+
+// Array: bastaki ogeyi cikartmak -> shift 
+firstItem = items.shift()  // ilk elemanı firstItem içerisine ekledik
+console.log(firstItem, items)
+
+// Array içerisindeki bir ogenin bilgisinin değiştirilmesi
+
+// ilk eleman
+items[0] = 5
+console.log(items)
+
+// son eleman 
+items[items.length-1] = 100
+console.log(items)
