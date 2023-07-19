53345abb571c675490f28e1d2b0b73bebc1e127d
safaanilatasoy <safaanilatasoy@gmail.com>
2023-07-19 15:52:58 +0300

    044

--- javascript-dersi/044-fetch-api.js
+++ javascript-dersi/044-fetch-api.js
@@ -1,6 +1,8 @@
 // Fetch API ile Çalışmak
 // Bir veri sunucudan geldikten sonra işlem yapabilmek ve sunucudan veri çekebilmek için kullanılır.
 
+
+// JSON Dosyasından Veri Çekmek
 fetch("data/settings.json").then(
     response => {
         return response.json();
@@ -8,3 +10,17 @@ fetch("data/settings.json").then(
     console.log(responseJson);
     console.log(responseJson.userName);
 })
+
+let userListDOM = document.querySelector("#userList");
+
+// API Üzerinden Veri Çektik
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then((response) => response.json())
+  .then((responseJson) => 
+  {
+    responseJson.forEach(item => {
+        let liDOM = document.createElement("li");
+        liDOM.innerHTML = item.title;
+        userListDOM.appendChild(liDOM);
+    })
+  });
