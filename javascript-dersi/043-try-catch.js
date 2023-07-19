502af28170955b52ce4ddc68dc7f0827e3ebe97a
safaanilatasoy <safaanilatasoy@gmail.com>
2023-07-19 15:35:46 +0300

    043

--- javascript-dersi/043-try-catch.js
+++ javascript-dersi/043-try-catch.js
@@ -1,8 +1,17 @@
 // Hata yönetimi için try catch kullanımı
 
-let items;
+let products;
 
-items.forEach(item => console.log(item));
+try{
+    products.forEach((item) => console.log(item));
+} catch(error){
+    console.log("error: ", error);
+    // products = [1,2,3,4];
+    // products.forEach((item) => console.log(item));
+}
+
+
+
 
 console.log("hata yonetimi duzgun calisiyor");
 
