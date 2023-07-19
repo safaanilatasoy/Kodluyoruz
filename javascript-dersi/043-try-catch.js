4601bfb648244637a6818bc2bb2adb6d222200e8
safaanilatasoy <safaanilatasoy@gmail.com>
2023-07-19 15:30:08 +0300

    043

---
+++ javascript-dersi/043-try-catch.js
@@ -0,0 +1,11 @@
+// Hata yönetimi için try catch kullanımı
+
+let items;
+
+items.forEach(item => console.log(item));
+
+console.log("hata yonetimi duzgun calisiyor");
+
+
+let info = "kodluyoruz";
+console.log(info);
