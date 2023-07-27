9068e09194614e841d237b57d8a0c7442947bef9
safaanilatasoy <safaanilatasoy@gmail.com>
2023-07-27 16:07:59 +0300

    döngülerde key prop u kullanımı

--- React/hello-react/src/App.js
+++ React/hello-react/src/App.js
@@ -5,11 +5,32 @@ import User from './component/User';
 
 import './App.css';
 
+const friends = [
+  {
+    id:1,
+    name: "Ahmet",
+  },
+  {
+    id: 2,
+    name: "Tayfun",
+  },
+  {
+    id:3,
+    name: "Gökhan",
+  },
+];
+
 
 function App() {
   return (
     <>
-      <User name="Anıl" surname="Atasoy" isLoggedIn={true}/>
+      <User 
+      name="Anıl" 
+      surname="Atasoy" 
+      isLoggedIn={true} 
+      age={24}
+      friends={friends}
+      />
     
     </>
   );
--- React/hello-react/src/component/User.js
+++ React/hello-react/src/component/User.js
@@ -1,10 +1,20 @@
-function User({name, surname, isLoggedIn}){
+function User({name, surname, isLoggedIn, age,friends}){
     return (
-      <h1>
-        {isLoggedIn
-          ? `Hoşgeldin ${name} ${surname}`
-          : "Lütfen giriş yapın"}
-      </h1>
+      <>
+        <h1>
+          {isLoggedIn
+            ? `Hoşgeldin ${name} ${surname} (${age})`
+            : "Lütfen giriş yapın"}
+        </h1>
+
+        <h2>Arkadaş Listesi</h2>
+        {
+           friends.map((friend) => (
+           <div key={friend.id}>
+             {friend.name}
+            </div>
+            ))}
+      </>
     );
 }
 
