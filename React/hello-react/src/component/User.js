9dc823e4498018da07477002bd002a8b88a3cfd5
safaanilatasoy <safaanilatasoy@gmail.com>
2023-08-05 13:12:58 +0300

    Prop Types

--- React/hello-react/src/component/User.js
+++ React/hello-react/src/component/User.js
@@ -1,3 +1,6 @@
+import PropTypes from 'prop-types'
+
+
 function User({name, surname, isLoggedIn, age,friends}){
     return (
       <>
@@ -16,6 +19,14 @@ function User({name, surname, isLoggedIn, age,friends}){
             ))}
       </>
     );
+}
+
+User.propTypes = {
+  name: PropTypes.string,
+  surname: PropTypes.string,
+  isLoggedIn: PropTypes.bool,
+  age: PropTypes.number,
+  friends: PropTypes.array
 }
 
 export default User;
