ba26ecce1611cabfaf9056b0f81417d9f2f76160
safaanilatasoy <safaanilatasoy@gmail.com>
2023-08-06 16:49:08 +0300

    Array States Added

--- React/states/src/App.js
+++ React/states/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import {useState} from "react";
 
@@ -6,12 +5,26 @@ function App() {
   const [name, setName] = useState('Safa');
   const [age, setAge] = useState(24);
 
+  // Array States
+
+  const [friends, setFriends] = useState(['İlker','Berat']);
+
   return (
     <div className="App">
       <h1>Merhaba {name}!</h1>
       <h2>{age}</h2>
       <button onClick={() => setName("Anıl")}>Change Name</button>
       <button onClick={() => setAge(29)}>Change Age</button>
+
+      <hr />
+      <br />
+      <br />
+      <h2>My Friends</h2>
+      {friends.map((friend, index) => (
+        <div>{friend}</div>
+      ))}
+      <br />
+      <button onClick={() => setFriends([...friends,'Ayşe'])}>Add New Friend</button>
     </div>
   );
 }
