2b9a22ac2fae8c61fed4edf4a834d5567b6edb6d
safaanilatasoy <safaanilatasoy@gmail.com>
2023-08-18 16:56:16 +0300

    filtreleme işlemi eklendi

--- React/rehber-app/src/components/Contacts/List/index.js
+++ React/rehber-app/src/components/Contacts/List/index.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import { useState } from 'react'
 
 function List({contacts}) {
+    const [filterText, setFilterText] = useState('');
+
+    const filtered = contacts.filter((item) => {
+        return Object.keys(item).some((key) => 
+            item[key].toString().toLowerCase().includes(filterText.toLocaleLowerCase())
+        );
+    })
   return (
     <div>
+
+        <input 
+        placeholder="Filter Contact" 
+        value={filterText} 
+        onChange={(e) => setFilterText(e.target.value)}
+        />
+
       <ul>
         {
-            contacts.map((contact, i) =>(
+            filtered.map((contact, i) =>(
 
             <li ket={i}>{contact.fullname}</li>
 
--- React/rehber-app/src/components/Contacts/index.js
+++ React/rehber-app/src/components/Contacts/index.js
@@ -3,7 +3,24 @@ import List from './List'
 import Form from './Form'
 
 function Contacts() {
-  const [contacts, setContacts] = useState([]);
+  const [contacts, setContacts] = useState([
+    {
+      fullname: "Anıl",
+      phone_number: "123123",
+    },
+    {
+      fullname: "Doğukan",
+      phone_number: "124233",
+    },
+    {
+      fullname: "ilker",
+      phone_number: "214545",
+    },
+    {
+      fullname: "Emre",
+      phone_number: "433523",
+    },
+  ]);
 
   useEffect(() => {
     console.log(contacts);
