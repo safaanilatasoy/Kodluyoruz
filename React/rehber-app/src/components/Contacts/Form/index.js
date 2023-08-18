2e50b846120ad0ec62b9c243c190dcd0ba46371a
safaanilatasoy <safaanilatasoy@gmail.com>
2023-08-18 16:26:19 +0300

    form component kodları düzenlendi...

--- React/rehber-app/src/components/Contacts/Form/index.js
+++ React/rehber-app/src/components/Contacts/Form/index.js
@@ -1,11 +1,55 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
 
-function Form() {
+
+const initialFormValues = {fullname: "", phone_number: ""};
+
+function Form({addContact, contacts}) {
+    const [form, setForm] = useState(initialFormValues);
+    
+    useEffect(() => {
+        setForm(initialFormValues);
+
+    }, [contacts]);
+
+    const onChangeInput = (e) => {
+        setForm({...form, [e.target.name]: e.target.value});
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        if(form.fullname === '' || form.phone_number === ''){
+            return false;
+        }
+        addContact([...contacts, form ]);
+
+    };
+
   return (
-    <div>
-      Form
-    </div>
-  )
+    <form onSubmit={onSubmit}>
+      <div>
+        <input 
+        name="fullname" 
+        placeholder="Fullname"  
+        onChange={onChangeInput}
+        value={form.fullname}
+        
+        />
+      </div>
+      <div>
+        <input 
+        name="phone_number" 
+        placeholder="Phone Number" 
+        onChange={onChangeInput}
+        value={form.phone_number}
+        />
+      </div>
+
+      <div>
+        <button>Add</button>
+      </div>
+    </form>
+  );
 }
 
 export default Form
