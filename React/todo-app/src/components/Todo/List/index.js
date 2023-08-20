c7f5b30daeb1550d16c935a6b445386a55203fa3
safaanilatasoy <safaanilatasoy@gmail.com>
2023-08-21 01:24:08 +0300

    main

--- React/todo-app/src/components/Todo/List/index.js
+++ React/todo-app/src/components/Todo/List/index.js
@@ -1,32 +1,29 @@
-import React from 'react'
+import { useState } from 'react'
 
 function List({todos}) {
+	
+
   return (
     <section className="main">
-		<input className="toggle-all" type="checkbox" />
-		<label for="toggle-all">
-			Mark all as complete
-		</label>
+      <input className="toggle-all" type="checkbox" />
+      <label for="toggle-all">Mark all as complete</label>
 
-		<ul className="todo-list">
-			{
-        todos.map((todos, i) => (
+      <ul className="todo-list">
+        {todos.map((todos, i) => (
           <li key={i} className="">
-				<div className="view">
-					<input className="toggle" type="checkbox" />
-					<label>{todos.newTodo}</label>
-					<button className="destroy"></button>
-				</div>
-			</li>
-        ))
-      }
-			
-			
-		</ul>
-	</section>
-
-
-  )
+            <div className="view">
+              <input
+                className="toggle"
+                type="checkbox"
+              />
+              <label>{todos.newTodo}</label>
+              <button className="destroy"></button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
 
 export default List
