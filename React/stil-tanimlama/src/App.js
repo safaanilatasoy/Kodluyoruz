677f982d45058c0ddfd644c798a5bdd03a3759de
safaanilatasoy <safaanilatasoy@gmail.com>
2023-08-22 18:10:54 +0300

    main

---
+++ React/stil-tanimlama/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <div style={{color:'red', backgroundColor:'#fff', paddingTop: 50}}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Velit, tempore. Voluptates voluptate esse, quo magni harum 
+          quis? Sint, fugit unde vitae sapiente repellat ex qui temporibus,
+           rerum quidem hic similique!
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
