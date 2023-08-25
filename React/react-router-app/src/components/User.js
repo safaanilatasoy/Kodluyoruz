5632bc65e36ae54976d504ffb066ce15fbead917
safaanilatasoy <safaanilatasoy@gmail.com>
2023-08-26 00:11:45 +0300

    main

--- React/react-router-app/src/components/User.js
+++ React/react-router-app/src/components/User.js
@@ -1,7 +1,17 @@
 import {useParams} from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 function User() {
     const {id} = useParams();
+    const [user, setUser] = useState({});
+
+    useEffect(() =>{
+        axious(`https://jsonplaceholder.typicode.com/users/${id}`)
+        .then(res => setUser(res.data)
+        
+        )
+    }, [])
+
   return (
   <div>
     <h1>User Detail</h1>
