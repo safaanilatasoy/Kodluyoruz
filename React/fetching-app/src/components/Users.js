1f58c340d43b87f2ab5b94747e0358c850105683
safaanilatasoy <safaanilatasoy@gmail.com>
2023-08-25 17:54:33 +0300

    axious

--- React/fetching-app/src/components/Users.js
+++ React/fetching-app/src/components/Users.js
@@ -1,29 +1,56 @@
 import {useEffect, useState} from 'react'
 
+import { axios } from 'axios';
+
+// fetch method
+// function Users() {
+
+//     const [users, setUsers] = useState([]);
+//     const [isLoading, setIsLoading] = useState(true);
+
+//     useEffect(() => {
+//         fetch("https://jsonplaceholder.typicode.com/users")
+//         .then(res => res.json())
+//         .then((data) => setUsers(data))
+//         .catch((e) => console.log(e))
+//         .finally(() => setIsLoading(false));
+
+//     }, []);
+//   return (
+//     <div>
+//       <h1>Users</h1>
+//       {isLoading && <div>Loading...</div>}
+//       {
+        
+//         users.map(user =>(
+//         <div key={user.id}>{user.name}</div>
+//         ))}
+//     </div>
+//   )
+// }
+
+// export default Users
+
+// axious method
 function Users() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    const [users, setUsers] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-
-    useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/users")
-        .then(res => res.json())
-        .then((data) => setUsers(data))
-        .catch((e) => console.log(e))
-        .finally(() => setIsLoading(false));
-
-    }, []);
+  useEffect(() => {
+    axious("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data))
+      .catch((e) => console.log(e))
+      .finally(() => setIsLoading(false));
+  }, []);
   return (
     <div>
       <h1>Users</h1>
       {isLoading && <div>Loading...</div>}
-      {
-        
-        users.map(user =>(
+      {users.map((user) => (
         <div key={user.id}>{user.name}</div>
-        ))}
+      ))}
     </div>
-  )
+  );
 }
 
-export default Users
+export default Users;
