aef104835995fdf5e8b2fce41eb7847c6f56e111
safaanilatasoy <safaanilatasoy@gmail.com>
2023-09-23 19:23:10 +0300

    class #6 || Arttırma ve Azaltma Operatörleri

---
+++ PHP/6-Operatorler/arttırma-ve-azaltma-operatorleri.php
@@ -0,0 +1,36 @@
+<?php
+
+// Arttırma Operatörü ----> ++
+$a = 5;
+$a++; // $a = $a + 1;
+echo $a; // Sonuç: 6
+
+## Sonradan Arttırma Operatörü ----> $a++
+$a = 5;
+echo $a++; // Sonuç: 5
+echo $a; // Sonuç: 6
+
+## Önceden Arttırma Operatörü ----> ++$a
+$a = 5;
+echo ++$a; // Sonuç: 6
+echo $a; // Sonuç: 6
+
+
+// Azaltma Operatörü ----> --
+$a = 5;
+$a--; // $a = $a - 1;
+echo $a; // Sonuç: 4
+
+## Sonradan Azaltma Operatörü ----> $a--
+$a = 5;
+echo $a--; // Sonuç: 5
+echo $a; // Sonuç: 4
+
+## Önceden Azaltma Operatörü ----> --$a
+$a = 5;
+echo --$a; // Sonuç: 4
+echo $a; // Sonuç: 4
+
+
+
+?>
