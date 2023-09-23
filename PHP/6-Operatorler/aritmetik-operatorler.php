49c59a9b01b0df95606dff2714fcf3071c9cd149
safaanilatasoy <safaanilatasoy@gmail.com>
2023-09-23 19:17:01 +0300

    class #6 || Aritmetik Operatörler

---
+++ PHP/6-Operatorler/aritmetik-operatorler.php
@@ -0,0 +1,17 @@
+<?php 
+
+// Toplama
+$toplam = 5 + 2;
+// Çıkarma
+$cikarma = 5 - 2;
+// Çarpma
+$carpma = 5 * 2;
+// Bölme
+$bolme = 5 / 2;
+// Mod Alma
+$mod = 5 % 2;
+// Üs Alma
+$us = 5 ** 2;
+
+
+?>
