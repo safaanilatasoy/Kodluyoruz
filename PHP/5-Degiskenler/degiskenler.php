14c04f42d13584fa96f5cab172e0194fca7bdf02
safaanilatasoy <safaanilatasoy@gmail.com>
2023-09-23 19:05:26 +0300

    class #5 || Değişkenler ve değişken tanımlamaları

---
+++ PHP/5-Degiskenler/degiskenler.php
@@ -0,0 +1,19 @@
+<?php 
+
+// PHP'de değişkenler $ işareti ile tanımlanır.
+// Değişkenlerin isimleri harf veya _ ile başlar.
+// Değişken isimleri sayı ile başlayamaz.
+// Değişken isimleri boşluk içeremez.
+// Değişken isimleri büyük küçük harf duyarlıdır.
+// Değişken isimleri Türkçe karakter içeremez.
+// Tüm veri tiplerini tutabilir 
+
+
+$isim = 'Anil ATASOY';
+echo $isim;
+
+// Değişkenleri sonradan değiştirebiliriz.
+
+$isim = 'Anil';
+echo $isim;
+?>
