97b68bae3cff368400a526784e61d5e7c9dfc09b
safaanilatasoy <safaanilatasoy@gmail.com>
2023-09-23 19:12:22 +0300

    class #5 || Veri Tipleri

---
+++ PHP/5-Degiskenler/veri-tipleri.php
@@ -0,0 +1,27 @@
+<?php 
+
+
+
+## Veri Tipleri ##
+// String
+$isim = 'Anil';
+// Integer
+$yas = 25;
+// Float(Double)
+$boy = 1.75;
+// Boolean(true, false)
+$isMarried = false;
+// Array(dizi)
+$sehirler = array('Ankara', 'İstanbul', 'İzmir');
+// Object(Nesne)
+$ogrenci = new stdClass();
+$ogrenci->isim = 'Anil';
+$ogrenci->soyisim = 'ATASOY';
+$ogrenci->yas = 25;
+// NULL
+$ogrenci = null;
+
+
+
+// gettype() fonksiyonu ile değişkenin veri tipini öğrenebiliriz.
+?>
