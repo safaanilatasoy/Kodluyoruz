92ea13beacfda7346e20f0d5ec18329317977239
safaanilatasoy <safaanilatasoy@gmail.com>
2023-09-23 19:09:29 +0300

    class #5 || Sabitler

---
+++ PHP/5-Degiskenler/sabitler.php
@@ -0,0 +1,13 @@
+<?php
+
+
+// Harf ve alt çizgi ile başlar.
+// Obje hariç tüm veri tiplerini tutabilir.
+// Sayı ile başlayamaz.
+// Türkçe karakter içerebilir (Tercih etmemek daha iyi çünkü kod evrensel olmalı).
+// Geleneksel olarak, sabit isimleri daima büyük harfle yazılır.
+// Sabitlerin değeri değiştirilemez.
+
+define('ISIM', 'Anil');
+echo ISIM;
+?>
