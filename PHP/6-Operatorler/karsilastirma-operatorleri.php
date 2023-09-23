b3f4ce0bc7c8110c01fc8008668fd16452b6745d
safaanilatasoy <safaanilatasoy@gmail.com>
2023-09-23 19:19:17 +0300

    class #6 || Karşılaştırma Operatörleri

---
+++ PHP/6-Operatorler/karsilastirma-operatorleri.php
@@ -0,0 +1,15 @@
+<?php 
+
+$a = 3; $b = 4;
+
+echo $a == $b; // Sonuç: false
+echo $a === $b; // Sonuç: false
+echo $a != $b; // Sonuç: true
+echo $a !== $b; // Sonuç: true
+echo $a < $b; // Sonuç: true
+echo $a > $b; // Sonuç: false
+echo $a <= $b; // Sonuç: true
+echo $a >= $b; // Sonuç: false
+echo $a <=> $b; // Sonuç: -1
+
+?>
