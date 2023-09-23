01ea373702cf54eeada6c56cd7bdbf895fd703c0
safaanilatasoy <safaanilatasoy@gmail.com>
2023-09-23 18:58:39 +0300

    main

---
+++ PHP/4-Html-icinde-PHP-kullanimi/html-ve-php.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Baslik</h1>
+    <? $selam = "Merhaba"; ?>
+    <h2><?= $selam ?></h2>
+    <h3><?php echo 'Merhaba Dünya!!' ?></h3>
+    
+</body>
+</html>
