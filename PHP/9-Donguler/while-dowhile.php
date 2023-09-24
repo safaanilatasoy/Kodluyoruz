c565d46a811d57043366aa09bb8ec96d34cc9546
safaanilatasoy <safaanilatasoy@gmail.com>
2023-09-24 16:52:24 +0300

    class #9 || while, do while döngüleri

---
+++ PHP/9-Donguler/while-dowhile.php
@@ -0,0 +1,21 @@
+<?php 
+
+# while loop
+
+$i = 1;
+while ($i <= 10) {
+    echo $i++;  /* yazdırılan değer $i
+                    değerinin artıştan önceki
+                    değeridir (sonda-arttırım) */
+}
+
+
+
+# do while loop
+
+$i = 0;
+do {
+    echo $i;
+} while ($i > 0);
+
+?>
