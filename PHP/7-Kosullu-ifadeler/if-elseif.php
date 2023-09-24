80741c95fc5d71e0f4d328c5d5d757017fa4a477
safaanilatasoy <safaanilatasoy@gmail.com>
2023-09-24 16:32:54 +0300

    class #7 || if else if

---
+++ PHP/7-Kosullu-ifadeler/if-elseif.php
@@ -0,0 +1,32 @@
+<?php 
+/**
+ * if($a >= $b){
+ * Kosul true ise burası çalışır.
+ * }else {
+ * kosul false ise burası çalışır.
+ * }
+ */
+
+  if($a == 1) 
+        echo "a eşittir 1e";
+    elseif($a == 2) 
+        echo "a eşittir 2ye";
+    elseif($a == 3) 
+        echo "a eşittir 3e";
+    else 
+        echo "a eşittir değildir 1e 2ye ve 3e";
+
+
+#   Ternary Operatör
+
+// (kosul) ? true ise : false ise :
+# Örnek
+
+// ($a < 5) ? echo "a küçüktür 5" : echo "a küçük değildir 5";  
+    
+//     // İfade uzun yazıldığında ağaşıda ki ifadeye karşılık gelir.
+//     if($a < 5) 
+//         echo "a küçüktür 5";
+//     else 
+//         echo "a küçük değildir 5";
+?>
