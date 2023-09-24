ce52176cdb7e8878964029b0f4246d45cfe7515e
safaanilatasoy <safaanilatasoy@gmail.com>
2023-09-24 16:38:01 +0300

    class #8 || tek boyutlu arrayler

--- PHP/7-Kosullu-ifadeler/switch-case.php
+++ PHP/7-Kosullu-ifadeler/switch-case.php
@@ -1,11 +1,5 @@
 <?php 
-if ($i == 0) {
-        echo "i eşittir 0";
-    } elseif ($i == 1) {
-        echo "i eşittir 1";
-    } elseif ($i == 2) {
-        echo "i eşittir 2";
-    }
+
     
     switch ($i) {
     case 0:
